a5ae56dc039f241e34366b077565a37079937648
ybchar <uiurihappy@naver.com>
2024-11-14 14:01:08 +0900

    Project init

---
+++ week_3/char-yb/week3/src/main/kotlin/com/sipe/week3/Week3Application.kt
@@ -0,0 +1,11 @@
+package com.sipe.week3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Week3Application
+
+fun main(args: Array<String>) {
+    runApplication<Week3Application>(*args)
+}
---
+++ week_3/week3_1.md
@@ -0,0 +1,4 @@
+## 코루틴 개념, 코루틴을 왜 쓰는가?
+- [코루틴 개념](https://en.wikipedia.org/wiki/Coroutine)
+- [코루틴 docs](https://kotlinlang.org/docs/coroutines-overview.html#documentation)
+
