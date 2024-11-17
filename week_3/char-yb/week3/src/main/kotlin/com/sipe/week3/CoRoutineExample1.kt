16e4bf51e2d7d7c3c59d40de814007307a68af97
ybchar <uiurihappy@naver.com>
2024-11-17 15:28:06 +0900

    코루틴과 스레드와의 차이

--- week_3/char-yb/week3/src/main/kotlin/com/sipe/week3/CoRoutineExample1.kt
+++ week_3/char-yb/week3/src/main/kotlin/com/sipe/week3/CoRoutineExample1.kt
@@ -1,5 +1,6 @@
 package com.sipe.week3
 
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -8,6 +9,10 @@ class CoRoutineExample1 {
 }
 
 fun main() = runBlocking { // this: CoroutineScope
+    helloWorld()
+}
+
+suspend fun helloWorld() = coroutineScope {
     launch { // launch a new coroutine and continue
         delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
         println("World!") // print after delay
---
+++ week_3/char-yb/week3/src/main/kotlin/com/sipe/week3/CoRoutineExample2.kt
@@ -0,0 +1,6 @@
+package com.sipe.week3
+
+class CoRoutineExample2 {
+}
+
+fun main() {}
