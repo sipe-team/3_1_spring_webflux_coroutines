d817060671f014490699160851a584b892489e83
ybchar <uiurihappy@naver.com>
2024-11-28 19:39:22 +0900

    Week 5 Project init

---
+++ week_5/char-yb/week5/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("plugin.jpa") version "1.9.25"
+}
+
+group = "com.sipe"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // JWT
+    val jjwtVersion = "0.11.5"
+    implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
+
+
+    // Spring Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-test")
+    implementation("org.springframework.security:spring-security-oauth2-client")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+// scripts 경로의 pre-commit hook 등록
+tasks.register("addGitPreCommitHook", DefaultTask::class) {
+    group = "setup"
+    description = "Install git hooks"
+    doLast {
+        val hooksDir = project.file(".git/hooks")
+        val scriptDir = project.file("scripts")
+        val preCommit = scriptDir.resolve("pre-commit")
+        preCommit.copyTo(hooksDir.resolve("pre-commit"), overwrite = true)
+        hooksDir.resolve("pre-commit").setExecutable(true)
+    }
+}
+
+// compileKotlin가 addGitPreCommitHook에 의존하도록 설정
+tasks.named("compileKotlin") {
+    dependsOn("addGitPreCommitHook")
+}
